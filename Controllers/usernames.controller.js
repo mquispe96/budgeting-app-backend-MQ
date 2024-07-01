ff519bd18d121b741f8f9e9bd195ca7a26122aa2
mquispe96 <mquispe@pursuit.org>
2024-07-01 10:24:05 -0400

    Improved routes and validations

--- Controllers/usernames.controller.js
+++ Controllers/usernames.controller.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import usernamesDB from '../Models/usernames.model.js';
 import {
+  usernameExists,
+  checkPassword,
   validateUsername,
   itsNewUsername,
   itsNewPassword,
@@ -9,28 +11,22 @@ import {
 const usernames = express.Router();
 
 usernames.get('/', (req, res) => res.json({error: 'Access Denied'}));
-usernames.get('/:username', (req, res) => {
-  const {username} = req.params;
-  const user = usernamesDB.find(user => user.username === username);
-  if (user) {
-    res.json({success: 'User found'});
-  } else {
-    res.status(404).json({error: 'User not found'});
-  }
+usernames.get('/login', usernameExists, checkPassword, (req, res) => {
+  res.json({success: 'Login successful'});
 });
 usernames.post('/', validateUsername, itsNewUsername, (req, res) => {
   const previousPasswords = [req.body.password];
   usernamesDB.push({...req.body, previousPasswords});
   res.json({success: 'User created'});
 });
-usernames.put('/:username', itsNewPassword, (req, res) => {
-  const {username} = req.params;
+usernames.put('/update', usernameExists, itsNewPassword, (req, res) => {
+  const {username, password} = req.body;
   const userIndex = usernamesDB.findIndex(user => user.username === username);
-  usernamesDB[userIndex].password = req.body.password;
+  usernamesDB[userIndex].password = password;
   res.json({success: 'User updated'});
 });
-usernames.delete('/:username', (req, res) => {
-  const {username} = req.params;
+usernames.delete('/delete', (req, res) => {
+  const {username} = req.body;
   const userIndex = usernamesDB.findIndex(user => user.username === username);
   if (userIndex > -1) {
     usernamesDB.splice(userIndex, 1);
--- Validation/validation.js
+++ Validation/validation.js
@@ -22,6 +22,24 @@ export const validateDataTypes = (req, res, next) => {
   return next();
 };
 
+export const usernameExists = (req, res, next) => {
+  const {username} = req.body;
+  const user = usernamesDB.find(user => user.username === username);
+  if (!user) {
+    return res.status(404).json({error: 'User not found'});
+  }
+  return next();
+};
+
+export const checkPassword = (req, res, next) => {
+  const {username, password} = req.body;
+  const user = usernamesDB.find(user => user.username === username);
+  if (user.password !== password) {
+    return res.status(400).json({error: 'Invalid password'});
+  }
+  return next();
+};
+
 export const validateUsername = (req, res, next) => {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const {username} = req.body;
@@ -41,10 +59,7 @@ export const itsNewUsername = (req, res, next) => {
 };
 
 export const itsNewPassword = (req, res, next) => {
-  const user = usernamesDB.find(user => user.username === req.params.username);
-  if (!user) {
-    return res.status(404).json({error: 'User not found'});
-  }
+  const user = usernamesDB.find(user => user.username === req.body.username);
   if (user.previousPasswords.includes(req.body.password)) {
     return res.status(400).json({error: 'Password already used'});
   }
