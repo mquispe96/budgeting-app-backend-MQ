7cd41186379837e6af5c007632f30d20f4d23d27
mquispe96 <mquispe@pursuit.org>
2024-07-02 18:36:24 -0400

    Fixed PUT and DELETE routes for usernames

--- Controllers/usernames.controller.js
+++ Controllers/usernames.controller.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {nanoid} from 'nanoid';
 import usernamesDB from '../Models/usernames.model.js';
 import {
   usernameExists,
@@ -13,21 +14,30 @@ const usernames = express.Router();
 usernames.get('/', (req, res) => res.json({error: 'Access Denied'}));
 usernames.post('/', validateUsername, itsNewUsername, (req, res) => {
   const previousPasswords = [req.body.password];
-  usernamesDB.push({...req.body, previousPasswords});
-  res.json({success: 'User created'});
+  usernamesDB.push({...req.body,id: nanoid(6), previousPasswords});
+  res.json(usernamesDB[usernamesDB.length - 1]);
 });
 usernames.post('/login', usernameExists, checkPassword, (req, res) => {
-  res.json({success: 'Login successful'});
-});
-usernames.put('/update', usernameExists, itsNewPassword, (req, res) => {
-  const {username, password} = req.body;
-  const userIndex = usernamesDB.findIndex(user => user.username === username);
-  usernamesDB[userIndex].password = password;
-  res.json({success: 'User updated'});
-});
-usernames.delete('/delete', usernameExists, checkPassword, (req, res) => {
   const {username} = req.body;
-  const userIndex = usernamesDB.findIndex(user => user.username === username);
+  const user = usernamesDB.find(user => user.username === username);
+  res.json(user);
+});
+usernames.put('/:id', (req, res) => {
+  const {id} = req.params;
+  const userIndex = usernamesDB.findIndex(user => user.id === id);
+  if (userIndex === -1) {
+    return res.status(404).json({error: 'User not found'});
+  }
+  const {password} = req.body;
+  usernamesDB[userIndex].password = password;
+  res.json({success: 'Password updated'});
+});
+usernames.delete('/:id', (req, res) => {
+  const {id} = req.params;
+  const userIndex = usernamesDB.findIndex(user => user.id === id);
+  if (userIndex === -1) {
+    return res.status(404).json({error: 'User not found'});
+  }
   usernamesDB.splice(userIndex, 1);
   res.json({success: 'User deleted'});
 });
--- Models/usernames.model.js
+++ Models/usernames.model.js
@@ -1,5 +1,8 @@
+import {nanoid} from 'nanoid';
+
 export default [
   {
+    id: nanoid(6),
     username: 'mquispe@pursuit.org',
     password: 'Pursuit@2024',
     previousPasswords: ['Pursuit@2024']
